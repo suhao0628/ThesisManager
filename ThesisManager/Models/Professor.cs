abcadc9dc017094fd9783912eef29887c3f7485b
suhao <suhaoxb@gmail.com>
2024-04-23 14:52:17 +0700

    Create Professor and Student entity

---
+++ ThesisManager/Models/Professor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ThesisManager.Models
+{
+    public class Professor
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ ThesisManager/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ThesisManager.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
