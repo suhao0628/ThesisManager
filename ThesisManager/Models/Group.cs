7e611705180c48eaca525a77486d5cebe8ba1fbb
suhao <suhaoxb@gmail.com>
2024-04-23 14:54:43 +0700

    Create Group entity

---
+++ ThesisManager/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThesisManager.Models
+{
+    public class Group
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string GroupName { get; set; }
+        public List<Student>? Students { get; set; }
+        public string? BGColor { get; set; } = "bg-primary";
+    }
+}
